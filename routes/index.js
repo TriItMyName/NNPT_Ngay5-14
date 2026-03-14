98b4f2ad62631f4ee0ffbdedd1b662e87787008a
Trần Trọng Trí <161337259+TriItMyName@users.noreply.github.com>
2026-03-14 15:12:56 +0700

    final

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).json({
+    status: 'ok',
+    service: 'NNPT_Ngay5-14',
+    message: 'server is running'
+  });
 });
 
 module.exports = router;
